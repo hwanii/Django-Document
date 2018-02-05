b996a3dacdf06b6866faa5dc57a26ee1de01cb4e
Ryu Taehwan <yth3260@gmail.com>
2018-02-05 17:02:17 +0900

    기본형 중개보델 사용법 예제 추가

--- django/many_to_many/admin.py
+++ django/many_to_many/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
-from many_to_many.models import Pizza, Topping
+from many_to_many.models import Pizza, Topping, Post, User, PostLike
 
 admin.site.register(Pizza)
 admin.site.register(Topping)
+admin.site.register(Post)
+admin.site.register(User)
+admin.site.register(PostLike)
--- django/many_to_many/models.py
+++ django/many_to_many/models.py
@@ -1,4 +1,7 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 
 class Topping(models.Model):
@@ -14,3 +17,49 @@ class Pizza(models.Model):
 
     def __str__(self):
         return self.name
+
+
+# Extra fields on many-to-many relationships
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    like_users = models.ManyToManyField(
+        'User',
+        through='PostLike',
+        # MTM으로 연결된 반대편에서 (지금의 경우 특정 User가 좋아요 누른 Post목록을 가져오고 싶은 경우)
+        # 자동 생성되는 역방향 매니저 일므인 post_set대신 like_posts라는 이름을 사용하도록 한다.
+        related_name='like_posts',
+    )
+
+    def __str__(self):
+        return self.title
+
+
+class User(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class PostLike(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE
+    )
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+    )
+    created_date = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    def __str__(self):
+        return '"{title}"글의 좋아요({name}, {date})'.format(
+            title=self.post.title,
+            name=self.user.name,
+            date=datetime.strftime(
+                # timezone.make_naive(self.created_date), '%Y.%m.%d'),
+                timezone.localtime(self.created_date), '%Y.%m.%d'),
+        )
