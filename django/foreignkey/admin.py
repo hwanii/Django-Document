20fc74e8cf9cc9d6ca54ea38b6870ae9abd81d32
Ryu Taehwan <yth3260@gmail.com>
2018-02-05 15:14:25 +0900

    fereignkey field에서 primary_key를 따로 지정한 경우 실습

--- django/foreignkey/admin.py
+++ django/foreignkey/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Manufacturer,Car
+from .models import Manufacturer, Car, Person, Pokemon, Type
 
 admin.site.register(Manufacturer)
 admin.site.register(Car)
+admin.site.register(Person)
+admin.site.register(Pokemon)
+admin.site.register(Type)
--- django/foreignkey/models.py
+++ django/foreignkey/models.py
@@ -1,14 +1,44 @@
 from django.db import models
 
+
 class Manufacturer(models.Model):
-    name = models.CharField("Manufacturer's name",max_length=50)
+    name = models.CharField("Manufacturer's name", max_length=50)
 
     def __str__(self):
         return self.name
 
+
 class Car(models.Model):
     manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
-    name = models.CharField("Car's name",max_length=60)
+    name = models.CharField("Car's name", max_length=60)
 
     def __str__(self):
         return f'{self.manufacturer.name} {self.name}'
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=60)
+    # 자기자신을 다대일로 연결하는 필드
+    # 비어있어도 무관, 연결된 객체가 삭제되면 함께 삭제되지 않고 해당필드를 비움
+    teacher = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Type(models.Model):
+    name = models.CharField(max_length=60)
+    type_number = models.IntegerField(primary_key=True)
+
+    def __str__(self):
+        return f'{self.type_number} | {self.name}'
+
+
+class Pokemon(models.Model):
+    dex_number = models.IntegerField(primary_key=True)
+
+    type = models.ForeignKey(Type, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.dex_number}, {self.name} {self.type.name}'
