1f60dd4be12a21447d7279adc7edcdbafc067cde
Ryu Taehwan <yth3260@gmail.com>
2018-02-06 11:02:40 +0900

    자기자신을 MTM field로 갖는 예제 모델 추가

--- django/many_to_many/admin.py
+++ django/many_to_many/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 
-from many_to_many.models import Pizza, Topping, Post, User, PostLike
+from many_to_many.models import (
+    # basic
+    Pizza, Topping,
+    # intermediate
+    Post, User, PostLike,
+    # self
+    FacebookUser,
+)
 
 admin.site.register(Pizza)
 admin.site.register(Topping)
 admin.site.register(Post)
 admin.site.register(User)
 admin.site.register(PostLike)
+admin.site.register(FacebookUser)
