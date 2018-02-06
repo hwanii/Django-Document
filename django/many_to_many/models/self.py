0a285e5bf68d3d66ac25a54ae45fef1a2dd8d256
Ryu Taehwan <yth3260@gmail.com>
2018-02-06 11:37:45 +0900

    FacebookUser의 __str__ 메서드 수정

--- django/many_to_many/models/self.py
+++ django/many_to_many/models/self.py
@@ -4,10 +4,18 @@ __all__ = (
     'FacebookUser',
 )
 
-
+# 누구를 친구 추가 했으면 서로 친구가 됨 -> 내가 너의 친구면 너도 나의 친구 -> 동등한 관계가 됨.
 class FacebookUser(models.Model):
     name = models.CharField(max_length=50)
     friends = models.ManyToManyField('self')
 
     def __str__(self):
-        return self.name
+        # list comprehension 사용
+        friends = ', '.join([friend.name for friend in self.friends.all()])
+
+        # Manager의 values_list를 사용
+        # DB에서 모든 friends의 'name'필드의 값만 가져옴
+        friends = ', '.join(self.friends.values_list('name', flat=True))
+
+        return f'{self.name} (친구: {friends})'
+
---
+++ django/many_to_many/models/symmetrical.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Facebook과는 다르게 내가 팔로잉한다 해서 상대방이 나를 팔로잉 하는 것은 아님.
+class InstagramUser(models.Model):
+    name = models.CharField(max_length=50)
+    following = models.ManyToManyField(
+        'self',
+        # 대칭관계가 아님
+        symmetrical=False,
+    )
+
+    def __str__(self):
+        return self.name
