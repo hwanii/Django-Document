34134d0d3bd54e8e0088e3d59040c500e2f044fc
Ryu Taehwan <yth3260@gmail.com>
2018-02-06 12:23:59 +0900

    MTM에서 대칭이 False이며 중개모델을 쓰는 예제 추가

--- django/many_to_many/models/symmetrical.py
+++ django/many_to_many/models/symmetrical.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Facebook과는 다르게 내가 팔로잉한다 해서 상대방이 나를 팔로잉 하는 것은 아님.
 class InstagramUser(models.Model):
     name = models.CharField(max_length=50)
@@ -7,7 +8,9 @@ class InstagramUser(models.Model):
         'self',
         # 대칭관계가 아님
         symmetrical=False,
+        related_name='followers'
     )
 
     def __str__(self):
         return self.name
+
