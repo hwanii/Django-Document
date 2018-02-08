60ad4e68aa6947351454b335d2a7d0c8b5491c6a
Ryu Taehwan <yth3260@gmail.com>
2018-02-08 15:23:56 +0900

    추상클래스 실습을 위한 model 작성

--- django/inheritance/abstract_base_classes/models.py
+++ django/inheritance/abstract_base_classes/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class CommonInfo(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.PositiveIntegerField()
+
+    # 추상클래스이기 때문에 objects가 생성이 안됨.
+    class Meta:
+        abstract = True
+
+
+class Student(CommonInfo):
+    home_group = models.CharField(max_length=5)
