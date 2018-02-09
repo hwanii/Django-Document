4219161404096307bc033a41433cf11096d675fb
Ryu Taehwan <yth3260@gmail.com>
2018-02-09 15:19:19 +0900

    proxy_manager model 수정

--- django/inheritance/proxy_manager/models.py
+++ django/inheritance/proxy_manager/models.py
@@ -18,6 +18,7 @@ class NewManager(Manager):
 
 # 커스텀 매니저를 직접 자신의 속성으로 갖는 MyPerson1
 class MyPerson1(Person):
+    secondary = NewManager()
     class Meta:
         proxy = True
 
@@ -32,6 +33,6 @@ class ExtraManagerModel(models.Model):
 
 # 커스텀 매니저를 갖는 ABC모델을 상속 받는 MyPerson2
 # (간접적으로 secondary라는 Manager를 갖게됨)
-class MyPerson2(Person):
+class MyPerson2(Person, ExtraManagerModel):
     class Meta:
         proxy = True
